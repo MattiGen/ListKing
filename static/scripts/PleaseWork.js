b3c935f39981d940f270e14ca97c1deb0603ed8f
MattiGen <meg_g@rogers.com>
2018-06-07 09:50:21 -0400

    Thursday June 7

--- static/scripts/PleaseWork.js
+++ static/scripts/PleaseWork.js
@@ -1,2 +1,10 @@
-let dict=[['us1',12],['us2',9],['us3',6]];
-console.log(dict[0][0]);
+let dict=[{'us1':12},['us2',9],['us3',6]];
+//This gets each items keys
+console.log(Object.keys(dict[0]));
+//This Gets the scores
+console.log(dict[0].us1);
+
+
+let str='Hello,my,name,is';
+let res=str.split(',');
+console.log(res);
